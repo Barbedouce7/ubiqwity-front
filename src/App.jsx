5df5a7c58f8f698e70adf53dc478e28e92704e35
barbedouce <barbedouce7@pm.me>
2025-01-26 21:30:41 +0100

    tests

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
     </button>
   </div>
   <div class="flex-1">
-    <a class="btn btn-ghost text-xl">daisyUI</a>
+    <a class="btn btn-ghost text-xl">Ubi</a>
   </div>
   <div class="flex-none">
     <button class="btn btn-square btn-ghost">
