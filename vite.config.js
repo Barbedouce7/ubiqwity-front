d2e3821f17516243bbaddde951db5b846ee69bac
barbedouce <barbedouce7@pm.me>
2025-01-27 17:19:28 +0100

    fix domain CORS

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://barbedouce7.github.io/ubiqwity-front/',
+  base: 'https://ubi.hiddenlabs.cc',
 })
