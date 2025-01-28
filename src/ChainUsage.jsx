a5c8ed0d04af6e681ca8c9eff790e29a2b4ee237
barbedouce <barbedouce7@pm.me>
2025-01-28 15:50:22 +0100

    c

--- src/ChainUsage.jsx
+++ src/ChainUsage.jsx
@@ -16,17 +16,17 @@ const ChainUsage = ({ data }) => {
       <CardContent className="bg-slate-900">
         <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "8px" }}>
           <div>
-            <Typography variant="body2" style={{ color: getColor(data.average_5min) }}>
+            <Typography variant="body2" style={{ color: getColor(data[0].average_5min) }}>
               5 Min : {data.average_5min}%
             </Typography>
           </div>
           <div>
-            <Typography variant="body2" style={{ color: getColor(data.average_1h) }}>
+            <Typography variant="body2" style={{ color: getColor(data[0].average_1h) }}>
               1 Hour : {data.average_1h}%
             </Typography>
           </div>
           <div>
-            <Typography variant="body2" style={{ color: getColor(data.average_24h) }}>
+            <Typography variant="body2" style={{ color: getColor(data[0].average_24h) }}>
               24 Hour : {data.average_24h}%
             </Typography>
           </div>
