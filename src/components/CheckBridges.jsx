0aafd18f8b141158e12071435188d46e513f2ad2
barbedouce <barbedouce7@pm.me>
2025-02-26 16:16:38 +0100

    ,

--- src/components/CheckBridges.jsx
+++ src/components/CheckBridges.jsx
@@ -3,6 +3,8 @@ import { useTheme } from '../utils/useTheme';
 import { QuestionMarkCircleIcon, ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Tooltip } from 'react-tooltip';
 import logoRosen from '/rosen-logo.svg';
+import { Link } from 'react-router-dom'; 
+import { shortener } from '../utils/utils';
 
 const CheckBridges = ({ metadata }) => {
   if (!metadata || !Array.isArray(metadata) || metadata.length === 0) {
@@ -12,6 +14,10 @@ const CheckBridges = ({ metadata }) => {
   const transactionData = metadata[0];
   
   if (!transactionData.json_metadata || transactionData.label !== "0") {
+    return null;
+  }
+  const requiredFields = ['to', 'bridgeFee', 'networkFee', 'toAddress', 'fromAddress'];
+  if (!requiredFields.every(field => field in transactionData.json_metadata)) {
     return null;
   }
 
@@ -38,6 +44,12 @@ const CheckBridges = ({ metadata }) => {
     }
   };
 
+ const needsLink = (address) => {
+    const prefixes = ['addr', 'ddz', 'Ae'];
+    return typeof address === 'string' && prefixes.some(prefix => address.startsWith(prefix));
+  };
+ const displayFromAddress = Array.isArray(fromAddress) ? fromAddress[0] : fromAddress;
+
   return (
     <div className="card w-full max-w-2xl bg-base-100 shadow-xl mx-auto">
       <div className="card-body p-4 sm:p-6">
@@ -58,26 +70,44 @@ const CheckBridges = ({ metadata }) => {
         <div className="space-y-4 mt-4">
           <div className="flex flex-col md:flex-row md:justify-between">
             <span className={`${colors.text} font-medium`}>From:</span>
-            <p className="font-mono text-sm break-all mt-1 md:mt-0 md:max-w-[60%]">
-              {Array.isArray(fromAddress) ? fromAddress[0] : fromAddress}
+            <p className="font-mono text-sm break-all mt-1 pt-1 md:mt-0">
+              {needsLink(displayFromAddress) ? (
+                <Link 
+                  className="hover:opacity-90 transition-opacity text-primary" 
+                  to={`/wallet/${displayFromAddress}`}
+                >
+                  {shortener(displayFromAddress)}
+                </Link>
+              ) : (
+                displayFromAddress
+              )}
             </p>
           </div>
 
           <div className="flex flex-col md:flex-row md:justify-between">
             <span className={`${colors.text} font-medium`}>To:</span>
-            <p className="font-mono text-sm break-all mt-1 md:mt-0 md:max-w-[60%]">
-              {toAddress}
+            <p className="font-mono text-sm break-all mt-1 pt-1 md:mt-0">
+              {needsLink(toAddress) ? (
+                <Link 
+                  className="hover:opacity-90 transition-opacity" 
+                  to={`/wallet/${toAddress}`}
+                >
+                  {shortener(toAddress)}
+                </Link>
+              ) : (
+                toAddress
+              )}
             </p>
           </div>
 
           <div className="flex flex-col md:flex-row md:justify-between">
             <span className={`${colors.text} font-medium`}>Bridge Fee:</span>
-            <p className="font-mono text-sm break-all mt-1 md:mt-0 md:max-w-[50%] flex items-center justify-center gap-1 ">
+            <p className="font-mono text-sm break-all mt-1 md:mt-0 md:max-w-[50%] flex items-center justify-center gap-1">
               {formatFee(bridgeFee)}
               <QuestionMarkCircleIcon 
                 data-tooltip-id="fees-tooltip"
                 data-tooltip-content="RosenBridge fees are deducted in the currency sent by the user. At this time we display the fees without decimal adjustments."
-                className="h-5 w-5 text-gray-500 cursor-pointer"
+                className="h-5 w-5 text-base-content cursor-pointer"
               />
             </p>
           </div>
@@ -88,8 +118,8 @@ const CheckBridges = ({ metadata }) => {
               {formatFee(networkFee)}
               <QuestionMarkCircleIcon 
                 data-tooltip-id="fees-tooltip"
-                data-tooltip-content="RosenBridge fees are deducted in the currency sent by the user."
-                className="h-5 w-5 text-gray-500 cursor-pointer"
+                data-tooltip-content="RosenBridge fees are deducted in the currency sent by the user. At this time we display the fees without decimal adjustments."
+                className="h-5 w-5 text-base-content cursor-pointer"
               />
             </p>
           </div>
