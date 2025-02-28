4560c9c088c310639b35548582683d753b416703
barbedouce <barbedouce7@pm.me>
2025-02-28 12:50:49 +0100

    Save uncommited changes (WIP)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wasm()],
   base: 'https://ubiqwity.hiddenlabs.cc',
-})
+  build: {
+    target: 'esnext', // Définit la cible pour la construction finale
+  },
+  esbuild: {
+    target: 'esnext', // Pour le transpileur esbuild pendant le dev
+  },
+  optimizeDeps: {
+    exclude: ['@fabianbormann/cardano-peer-connect'], // Toujours pertinent si tu réinstalles ce package
+  },
+});
